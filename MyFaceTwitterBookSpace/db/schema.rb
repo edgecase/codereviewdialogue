ffa36acbbe77bc73f311fa5d7adf7343ad0de978
Jim Weirich <jim.weirich@gmail.com>
2009-03-24 18:41:11 +0000

    Added schema to source control.

--- MyFaceTwitterBookSpace/db/schema.rb
+++ MyFaceTwitterBookSpace/db/schema.rb
@@ -9,12 +9,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20081031211605) do
+ActiveRecord::Schema.define(:version => 20081108133319) do
 
   create_table "friends", :force => true do |t|
     t.string  "first_name"
     t.string  "last_name"
     t.integer "user_id"
+  end
+
+  create_table "groups", :force => true do |t|
+    t.string   "name"
+    t.integer  "cool_value"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", :force => true do |t|
